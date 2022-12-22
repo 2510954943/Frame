c27951f5ec35f92d9109ce28f299add3811cf00f
cai <2510954943@qq.com>
2022-12-22 14:42:11 +0800

    添加注释

--- libs/Log.php
+++ libs/Log.php
@@ -4,6 +4,11 @@ namespace libs;
 use libs\Config;
 
 class Log{
+
+	/**
+	 * 打开当日日志
+	 * @return Resource
+	 */
 	public function openLog(){
 		$month=date('Ym');
 		$dir=dirname(dirname(__FILE__)).'\runtime\\'.$month;
@@ -16,11 +21,25 @@ class Log{
 			return fopen($file,'a');
 		}
 	}
+
+	/**
+	 * 写入错误日志
+	 * @param string $type
+	 * @param string $code
+	 * @param string $msg
+	 * @return Boolean
+	 */
 	public function errofWrite($type,$code,$msg){
 		$times=date(DATE_ATOM);
 		$errorMsg="[$times]".' '.$type."[$code]".$msg;
 		$file=$this->openLog($errorMsg);
-		fwrite($file,$errorMsg);
+		$res=fwrite($file,$errorMsg);
 		fclose($file);
+		if($res){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
